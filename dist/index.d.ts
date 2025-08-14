e160b0821e168650308888abb529dfa0455ce68c
Lucas Mösch <lucasmoesch@live.de>
2025-08-14 14:50:17 +0200

    Removed 'dist' from git

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './lsx';
-export * from './lsx.common';
-export * from './lsx.drone';
-export * from './lsx.fluff';
-export * from './lsx.light';
-export * from './lsx.lockdown';
-export * from './lsx.power';
--- dist/lsx.common.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum LockState {
-    LOCK_STATE_EMPTY = 0,
-    LOCK_STATE_LOCKED = 1,
-    LOCK_STATE_UNLOCKED = 2,
-    UNRECOGNIZED = -1
-}
-export declare function lockStateFromJSON(object: any): LockState;
-export declare function lockStateToJSON(object: LockState): string;
